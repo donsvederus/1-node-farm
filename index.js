f9f38c670f699c738992ccee69eb689a33189b79
Don Svederus <svederus@gmail.com>
2022-05-22 16:18:51 -0600

    0.10.1 Non-blocking asynchorous way

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written');
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written');
+// Non-Blocking, Asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) =>  {
+    console.log(data);
+});
+console.log('Will read file!');
